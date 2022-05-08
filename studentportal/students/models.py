558a112c401059a363e15c3fd1f7a4f5b562ee9b
Celnet-hub <nwabuisicelestine@gmail.com>
2022-05-08 13:05:51 +0100

    created the student model and initial data

--- studentportal/students/models.py
+++ studentportal/students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create student's models here.
+
+class Student(models.Model):
+    first_name = models.CharField("First name", max_length=255)
+    last_name = models.CharField("Last name", max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    reg_no = models.TextField(blank=False, null=False)
+    address = models.TextField(blank=True, null=True)
+    current_level = models.IntegerField(blank=False, null=False)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
