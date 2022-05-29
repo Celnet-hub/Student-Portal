d663e60cc69319f4fce8921cf8493cb92dbe37b3
Celnet-hub <nwabuisicelestine@gmail.com>
2022-05-29 14:27:13 +0100

    modified static url

--- studentportal/settings.py
+++ studentportal/settings.py
@@ -143,13 +143,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    #point to static in React build folder
-    os.path.join(BASE_DIR, 'build/static'),
-   # os.path.join(BASE_DIR, '/app/static'),
+    os.path.join(BASE_DIR, 'build/static')
 ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     #point to static in React build folder
+#     os.path.join(BASE_DIR, 'build/static'),
+#    # os.path.join(BASE_DIR, '/app/static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
