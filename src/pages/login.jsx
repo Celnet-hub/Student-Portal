492c1a9a444bf8889b3414707260dfa343385ae4
Celnet-hub <nwabuisicelestine@gmail.com>
2022-06-18 18:00:27 +0100

    adchanged input field type in login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -28,9 +28,9 @@ export default class Login extends Component {
 									</label>
 									<input
 										id="matric-no"
-										name="email"
-										type="email"
-										autoComplete="email"
+										name="matric-no"
+										type="text"
+										autoComplete="matric-no"
 										required
 										className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 										placeholder="Matric No / Application No"
@@ -54,12 +54,6 @@ export default class Login extends Component {
 
 							<div className="flex items-center justify-between">
 								<div className="flex items-center">
-									{/* <input
-										id="remember-me"
-										name="remember-me"
-										type="checkbox"
-										className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-									/> */}
 									<label
 										htmlFor="remember-me"
 										className="m-1 block text-sm text-gray-900 font-bold"
