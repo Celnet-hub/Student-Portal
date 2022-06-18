27b002aa679d41ea5bec42ea78c604f2bd85127c
Celnet-hub <nwabuisicelestine@gmail.com>
2022-06-18 15:09:44 +0100

    designed the login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { LockClosedIcon } from "@heroicons/react/solid";
 export default class Login extends Component {
-    render() {
-        return (
-            <>
+	render() {
+		return (
+			<>
 				<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-					<div className="max-w-md w-full space-y-8">
+					<div className="max-w-lg w-full space-y-8">
 						<div>
 							<img
 								className="mx-auto h-12 w-auto"
@@ -14,31 +15,25 @@ export default class Login extends Component {
 							<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
 								Sign in to your account
 							</h2>
-							<p className="mt-2 text-center text-sm text-gray-600">
-								Or{" "}
-								<a
-									href="#"
-									className="font-medium text-indigo-600 hover:text-indigo-500"
-								>
-									start your 14-day free trial
-								</a>
-							</p>
+							<div>
+                            <LockClosedIcon className="h-25 w-25 text-gray-600" />
+                            </div>
 						</div>
 						<form className="mt-8 space-y-6" action="#" method="POST">
 							<input type="hidden" name="remember" defaultValue="true" />
 							<div className="rounded-md shadow-sm -space-y-px">
 								<div>
-									<label htmlFor="email-address" className="sr-only">
+									<label htmlFor="matric-no" className="sr-only">
 										Email address
 									</label>
 									<input
-										id="email-address"
+										id="matric-no"
 										name="email"
 										type="email"
 										autoComplete="email"
 										required
 										className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-										placeholder="Email address"
+										placeholder="Matric No / Application No"
 									/>
 								</div>
 								<div>
@@ -89,10 +84,10 @@ export default class Login extends Component {
 									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 								>
 									<span className="absolute left-0 inset-y-0 flex items-center pl-3">
-										{/* <LockClosedIcon
+										<LockClosedIcon
 											className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
 											aria-hidden="true"
-										/> */}
+										/>
 									</span>
 									Sign in
 								</button>
@@ -101,6 +96,6 @@ export default class Login extends Component {
 					</div>
 				</div>
 			</>
-        );
-    }
+		);
+	}
 }
