aa0d92bdfe5c63f94559226ca9312785c2f441ac
Celnet-hub <nwabuisicelestine@gmail.com>
2022-06-18 14:15:03 +0100

    designed the landing and sing-in page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+
 export default class LandingPage extends Component {
-    render() {
-        return (
-            <div>
-                <h3>React Landing Component</h3>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<>
+				<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+					<div className="max-w-md w-full space-y-8">
+						<div>
+							<img
+								className="mx-auto h-12 w-auto"
+								src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+								alt="Workflow"
+							/>
+							<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+								Welcome to the Student Portal
+							</h2>
+							<p className="mt-2 text-center text-sm text-gray-600">
+							
+								<a
+									href="#"
+									className="font-medium text-indigo-600 hover:text-indigo-500"
+								>
+									Please login to continue
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
