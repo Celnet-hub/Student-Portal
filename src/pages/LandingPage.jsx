6716914556c59b284c9bf129501c5218700be66e
Celnet-hub <nwabuisicelestine@gmail.com>
2022-06-18 15:58:03 +0100

    added background color to landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ export default class LandingPage extends Component {
 	render() {
 		return (
 			<>
-				<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+				<div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-r from-cyan-500 to-blue-300">
 					<div className="max-w-md w-full space-y-8">
 						<div>
 							<img
@@ -12,10 +12,10 @@ export default class LandingPage extends Component {
 								src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
 								alt="Workflow"
 							/>
-							<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+							<h2 className="mt-6 text-center text-5xl font-extrabold text-gray-900">
 								Welcome to the Student Portal
 							</h2>
-							<p className="mt-2 text-center text-sm text-gray-600">
+							<p className="mt-2 text-center text-md text-gray-600">
 							
 								<a
 									href="/login"
