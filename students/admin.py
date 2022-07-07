0f04f1cbe4a4d628316047610f9c300f8d6003bb
Celnet-hub <nwabuisicelestine@gmail.com>
2022-07-07 16:31:13 +0100

    added tables to databases

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Student, Course, Lecturer, Department, Faculty, FailedCourse
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'address', 'createdAt', 'current_level', 'reg_no', 'student_faculty', 'student_department')
+    list_filter = ('student_department',)
+    search_fields = ('first_name', 'last_name', 'email', 'phone', 'address', 'createdAt', 'current_level', 'reg_no', 'student_faculty', 'student_department')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'description', 'createdAt', 'lecturer', 'level', 'semester', 'credit_unit', 'deparment', 'faculty')
+    list_filter = ('deparment',)
+    search_fields = ('name', 'code', 'description', 'createdAt', 'lecturer', 'level', 'semester', 'credit_unit', 'deparment', 'faculty')
+
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'address', 'createdAt', 'lecturer_department', 'lecturer_faculty', 'rank_title')
+    list_filter = ('lecturer_department',)
+    search_fields = ('first_name', 'last_name', 'email', 'phone', 'address', 'createdAt', 'lecturer_department', 'lecturer_faculty', 'rank_title')
+
+# class DepartmentAdmin(admin.ModelAdmin):
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'description', 'createdAt', 'faculty', 'HOD', 'no_of_courses')
+    list_filter = ('faculty',)
+    search_fields = ('name', 'code', 'description', 'createdAt', 'faculty', 'HOD', 'no_of_courses')
+
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'description', 'createdAt',)
+    list_filter = ('name',)
+    search_fields = ('name', 'code', 'description', 'createdAt', 'no_of_departments')
+
+class FailedCourseAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'semester', 'year')
+    list_filter = ('semester', 'year')
+    search_fields = ('student', 'course', 'semester', 'year')
 
 # Register your models here.
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lecturer, LecturerAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(FailedCourse, FailedCourseAdmin)
--- students/models.py
+++ students/models.py
@@ -11,6 +11,75 @@ class Student(models.Model):
     address = models.TextField(blank=True, null=True)
     current_level = models.IntegerField(blank=False, null=False)
     createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+    student_faculty = models.ForeignKey('Faculty', on_delete=models.SET_NULL, blank=True, null=True)
+    student_department = models.ForeignKey('Department', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
-        return self.first_name
+        return self.first_name + " " + self.last_name
+
+# Create Courses Models
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+    lecturer = models.ForeignKey('Lecturer', on_delete=models.SET_NULL, null=True)
+    level = models.IntegerField(blank=False, null=False)
+    semester = models.IntegerField(blank=False, null=False)
+    credit_unit = models.IntegerField(blank=False, null=False)
+    deparment = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+    faculty = models.ForeignKey('Faculty', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Create Lecturers Models
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+    lecturer_department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+    lecturer_faculty = models.ForeignKey('Faculty', on_delete=models.SET_NULL, null=True)
+    rank_title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+# Create Departments Models
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+    faculty = models.ForeignKey('Faculty', on_delete=models.SET_NULL, null=True)
+    HOD = models.ForeignKey('Lecturer', on_delete=models.SET_NULL, null=True)
+    no_of_courses = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+# Create Faculties Models
+class Faculty(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+    no_of_departments = models.IntegerField(blank=False, null=False)
+    dean = models.ForeignKey('Lecturer', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Create Failed Courses Models
+class FailedCourse(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+    semester = models.IntegerField(blank=False, null=False)
+    year = models.IntegerField(blank=False, null=False)
+    createdAt = models.DateTimeField("cCreated At", auto_now_add=True)
+
+    def __str__(self):
+        return self.student.first_name + " " + self.student.last_name + " " + self.course.name
