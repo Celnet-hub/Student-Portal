0f29b45fb510b27f8d8266141631ffff5e18ff1b
Chidubem Nwabuisi <Nwabuisicelestine@gmail.com>
2022-07-18 04:08:19 +0100

    removed other links from the sidebar

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -499,65 +499,66 @@ export const stdlinks = [];
             },
           ],
         }, 
-        {
-          title: 'Apps',
-          links: [
-            {
-              name: 'calendar',
-              icon: <AiOutlineCalendar />,
-            },
-            {
-              name: 'kanban',
-              icon: <BsKanban />,
-            },
-            {
-              name: 'editor',
-              icon: <FiEdit />,
-            },
-            {
-              name: 'color-picker',
-              icon: <BiColorFill />,
-            },
-          ],
-        },
-        {
-          title: 'Charts',
-          links: [
-            {
-              name: 'line',
-              icon: <AiOutlineStock />,
-            },
-            {
-              name: 'area',
-              icon: <AiOutlineAreaChart />,
-            },
+        // {
+        //   title: 'Apps',
+        //   links: [
+        //     {
+        //       name: 'calendar',
+        //       icon: <AiOutlineCalendar />,
+        //     },
+        //     {
+        //       name: 'kanban',
+        //       icon: <BsKanban />,
+        //     },
+        //     {
+        //       name: 'editor',
+        //       icon: <FiEdit />,
+        //     },
+        //     {
+        //       name: 'color-picker',
+        //       icon: <BiColorFill />,
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: 'Charts',
+        //   links: [
+        //     {
+        //       name: 'line',
+        //       icon: <AiOutlineStock />,
+        //     },
+        //     {
+        //       name: 'area',
+        //       icon: <AiOutlineAreaChart />,
+        //     },
       
-            {
-              name: 'bar',
-              icon: <AiOutlineBarChart />,
-            },
-            {
-              name: 'pie',
-              icon: <FiPieChart />,
-            },
-            {
-              name: 'financial',
-              icon: <RiStockLine />,
-            },
-            {
-              name: 'color-mapping',
-              icon: <BsBarChart />,
-            },
-            {
-              name: 'pyramid',
-              icon: <GiLouvrePyramid />,
-            },
-            {
-              name: 'stacked',
-              icon: <AiOutlineBarChart />,
-            },
-          ],
-        })
+        //     {
+        //       name: 'bar',
+        //       icon: <AiOutlineBarChart />,
+        //     },
+        //     {
+        //       name: 'pie',
+        //       icon: <FiPieChart />,
+        //     },
+        //     {
+        //       name: 'financial',
+        //       icon: <RiStockLine />,
+        //     },
+        //     {
+        //       name: 'color-mapping',
+        //       icon: <BsBarChart />,
+        //     },
+        //     {
+        //       name: 'pyramid',
+        //       icon: <GiLouvrePyramid />,
+        //     },
+        //     {
+        //       name: 'stacked',
+        //       icon: <AiOutlineBarChart />,
+        //     },
+        //   ],
+        // }
+        )
       
           
       }
